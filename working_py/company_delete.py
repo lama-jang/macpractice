cff3a47b5c00e88e5f7ed16bbb74e02250f0a213
lama-jang <jschw33@gmail.com>
2021-02-02 14:22:51 +0900

    company_delete.py complete

--- working_py/company_delete.py
+++ working_py/company_delete.py
@@ -11,26 +11,30 @@ companys = []
 for i in range(0, 2656):
     companys.append(col_1[i].value)
 
-# for cell in col_1:
-    # i = 2
-    # while i <= 2656:
-        # if str(cell.value) == ws.cell(row=i, column=9).value:
-            # target_counts += 1
-            # ws.delete_rows(i)
-        # else:
-            # i += 1
-
 target_counts = 0
 
-i = 2
-while i <= 8412:
+# i = 2
+# while i <= 8412:
+    # if ws.cell(row=i, column=8).value in companys:
+        # ws.delete_rows(i)
+        # target_counts += 1
+    # if ws.cell(row=i, column=8).value is None:
+        # break
+    # else:
+        # i += 1
+
+for i in range(8411, 1, -1):
     if ws.cell(row=i, column=8).value in companys:
-        target_counts += 1
         ws.delete_rows(i)
+        target_counts += 1
+    if ws.cell(row=i, column=8).value is None:
+        break
     else:
-        i += 1
+        pass
 
 print(target_counts)
+ws.delete_cols(9)
+
 wb.save("delete_file.xlsx")
 
 print(time.time()-start)
