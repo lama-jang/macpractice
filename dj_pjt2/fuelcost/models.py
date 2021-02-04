55f294237aaa73104931c21a56b7092b05cddae6
Lama Jang <jschw33@gmail.com>
2021-02-04 23:26:21 +0900

    Second fix fuelcost App

--- dj_pjt2/fuelcost/models.py
+++ dj_pjt2/fuelcost/models.py
@@ -9,8 +9,14 @@ class fuel_info(models.Model):
         return self.name
 
 class trip_info(models.Model):
-    traveler = models.ForeignKey(fuel_info, on_delete=models.CASCADE)
+    traveler = models.ForeignKey(fuel_info, on_delete=models.CASCADE, help_text='Select Your Name')
     date = models.DateField()
+
+    class Meta:
+        ordering = ['traveler']
 
     def __str__(self):
         return self.traveler
+
+    def get_absolute_url(self):
+        return reverse('model-detail-view', args=[str(self.id)])
--- dj_pjt2/fuelcost/views.py
+++ dj_pjt2/fuelcost/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import fuel_info
+from .models import fuel_info, trip_info
 
 def home(request):
-    output = fuel_info.name
+    output = trip_info.traveler
     return HttpResponse(output)
+
 
 # def cost_results(request, traveler):
