77282b8cf40254a21f7f39584de39b238630a6b6
Lama Jang <jschw33@gmail.com>
2021-02-17 23:08:49 +0900

    mac second modified

--- dj_pjt2/fuelcost/admin.py
+++ dj_pjt2/fuelcost/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Traveler, FuelInfo
 
-admin.site.register(Traveler)
-admin.site.register(FuelInfo)
+# admin.site.register(Traveler)
+# admin.site.register(FuelInfo)
+
+class TravelerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Traveler, TravelerAdmin)
+
+@admin.register(FuelInfo)
+class FuelInfo(admin.ModelAdmin):
+    list_display = ('name', 'car', 'efficiency')
+
 
--- dj_pjt2/fuelcost/views.py
+++ dj_pjt2/fuelcost/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import FuelInfo
 from django.template import loader
+from django.views import generic
 
 def home(request):
     traveler_name = FuelInfo.objects.all()
@@ -13,3 +14,11 @@ def calfuel(request, name_id):
     selected_traveler = get_object_or_404(FuelInfo, pk=name_id)
 
 # def cost_results(request, traveler):
+
+class TravelerListView(generic.ListView):
+    model = FuelInfo
+
+    def get_queryset(self, **kwargs):
+        context = super(TravelerListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
