3c5b0691a5b91c98acabeb754864012ad3b150ce
Lama Jang <jschw33@gmail.com>
2021-02-22 22:45:33 +0900

    mac modified 2.22

--- dj_pjt2/fuelcost/urls.py
+++ dj_pjt2/fuelcost/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'fuelcost'
 urlpatterns = [
     path('', views.TravelerListView.as_view(), name='home'),
-    path('<int:pk>/', views.TravelerDetailVeiw.as_view(), name='calfuel'),
+    # path('<int:pk>/', views.TravelerDetailVeiw.as_view(), name='calfuel'),
+    path('<int:name_id>/', views.traveler_detail, name='calfuel'),
 ]
--- dj_pjt2/fuelcost/views.py
+++ dj_pjt2/fuelcost/views.py
@@ -8,7 +8,11 @@ class TravelerListView(generic.ListView):
     template_name = "fuelcost/home.html"
     context_obejct_name = "traveler_list"
 
-class TravelerDetailVeiw(generic.DetailView):
-    model = FuelInfo
-    template_name = "fuelcost/calfuel.html"
-    context_object_name = "traveler_detail"
+# class TravelerDetailVeiw(generic.DetailView):
+#     model = FuelInfo
+#     template_name = "fuelcost/calfuel.html"
+#     context_object_name = "traveler_detail"
+
+def traveler_detail(request, name_id):
+    traveler = get_object_or_404(FuelInfo, pk=name_id)
+    return render(request, 'fuelcost/calfuel.html', {'traveler': traveler})
