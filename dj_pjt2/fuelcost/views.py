2a4b3e47b85b44e74a3021e612ec41f48749917a
lama-jang <jschw33@gmail.com>
2021-02-26 17:33:51 +0900

    window updates 2/26

--- dj_pjt2/fuelcost/views.py
+++ dj_pjt2/fuelcost/views.py
@@ -1,23 +1,38 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import FuelInfo
 from django.template import loader
-from django.views import generic
+# from django.views import generic
+from .forms import FuelInfoForm
 
-class TravelerListView(generic.ListView):
-    model = FuelInfo
-    template_name = "fuelcost/home.html"
-    context_obejct_name = "traveler_list"
+# def traveler_list(request):
+#     travelers = FuelInfo.objects.all()
+#     context = {'travelers':travelers}
+#     return render(request, 'fuelcost/home.html', context)
 
 def traveler_list(request):
-    travelers = FuelInfo.objects.all()
-    context = {'travelers':travelers}
-    return render(request, 'fuelcost/home.html', context)
+    form = FuelInfoForm()
+
+    if request.method == 'POST':
+        form = FuelInfoForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect(reverse('fuelcost:calfuel'))
+    errors = form.errors or None 
+    return render(request, 'fuelcost/home.html', {'form': form, 'errors': errors,})
+
+
+def traveler_detail(request, pk):
+    traveler = get_object_or_404(FuelInfo, pk=pk)
+    return render(request, 'fuelcost/calfuel.html', {'traveler': traveler})
+
+# class TravelerListView(generic.ListView):
+#     model = FuelInfo
+#     template_name = "fuelcost/home.html"
+#     context_obejct_name = "traveler_list"
 
 # class TravelerDetailVeiw(generic.DetailView):
 #     model = FuelInfo
 #     template_name = "fuelcost/calfuel.html"
 #     context_object_name = "traveler_detail"
 
-def traveler_detail(request, pk):
-    traveler = get_object_or_404(FuelInfo, pk=pk)
-    return render(request, 'fuelcost/calfuel.html', {'traveler': traveler})
